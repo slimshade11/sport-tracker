f9dbd7aa45c1da01f13885e4524d488fc55b4261
Piotr Krzaczkowski <piotr.krzaczkowski@embiq.com>
2022-05-24 22:39:35 +0200

    git add custom styles

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -4,9 +4,10 @@ import { SignUpComponent } from '@auth/components/sign-up/sign-up.component';
 import { LogInComponent } from '@auth/components/log-in/log-in.component';
 import { AuthViewComponent } from '@auth/auth-view/auth-view.component';
 import { AuthRoutingModule } from '@auth/auth-routing.module';
+import { ComponentsModule } from '@components/components.module';
 
 @NgModule({
   declarations: [SignUpComponent, LogInComponent, AuthViewComponent],
-  imports: [CommonModule, AuthRoutingModule],
+  imports: [CommonModule, AuthRoutingModule, ComponentsModule],
 })
 export class AuthModule {}
