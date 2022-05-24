f75c6c343dfbc5060292d023ed468015c0d834e5
Piotr Krzaczkowski <piotr.krzaczkowski@embiq.com>
2022-05-24 22:13:59 +0200

    add basic structure, add routing

---
+++ src/app/modules/auth/auth-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SignUpComponent } from '@auth/components/sign-up/sign-up.component';
+import { LogInComponent } from '@auth/components/log-in/log-in.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'sign-up',
+    pathMatch: 'full',
+  },
+  {
+    path: 'sign-up',
+    component: SignUpComponent,
+  },
+  {
+    path: 'log-in',
+    component: LogInComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class AuthRoutingModule {}
---
+++ src/app/modules/auth/auth.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SignUpComponent } from '@auth/components/sign-up/sign-up.component';
+import { LogInComponent } from '@auth/components/log-in/log-in.component';
+import { AuthViewComponent } from '@auth/auth-view/auth-view.component';
+import { AuthRoutingModule } from '@auth/auth-routing.module';
+
+@NgModule({
+  declarations: [SignUpComponent, LogInComponent, AuthViewComponent],
+  imports: [CommonModule, AuthRoutingModule],
+})
+export class AuthModule {}
